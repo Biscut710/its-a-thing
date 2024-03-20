8667a6636cbdd89c97504bd118e56b8407f1b887
Biscut710 <jmmichaeldev@gmail.com>
2024-03-20 11:44:50 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . 6 6 6 6 6 6 6 . . . . 
+    . . 6 6 7 7 7 7 7 7 7 6 6 . . 
+    . 6 6 7 7 7 8 8 8 7 7 7 6 6 . 
+    . 6 7 7 7 8 8 7 8 8 7 7 7 6 . 
+    . c 7 7 8 8 8 8 8 8 8 7 7 c . 
+    . c 9 7 8 7 7 7 7 7 8 7 9 c . 
+    . c 9 9 7 7 7 7 7 7 7 9 9 c . 
+    . c 6 6 9 9 9 9 9 9 9 6 6 c . 
+    c c 6 6 6 6 6 6 6 6 6 6 6 c c 
+    c d c c 6 6 6 6 6 6 6 c c d c 
+    c d d d c c c c c c c d d d c 
+    c c b d d d d d d d d d b c c 
+    c c c c c b b b b b c c c c c 
+    c c b b b b b b b b b b b c c 
+    . c c b b b b b b b b b c c . 
+    . . . c c c c c c c c c . . . 
+    `, SpriteKind.Player)
